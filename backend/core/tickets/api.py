e343dac2093719ece2113e60d461356179677722
Evgeniy Perov <evgeniy.perov.05@mail.ru>
2025-10-05 00:08:47 +0500

    backend: core: пагинация в tickets/list, возвращаются все тикеты с инфой о юзере, tickets/modify

--- backend/core/tickets/api.py
+++ backend/core/tickets/api.py
@@ -1,5 +1,4 @@
-import datetime
-
+from django.core.paginator import Paginator
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from rest_framework import status as status_code
@@ -45,12 +44,32 @@ class CreateTicket(BaseApiView):
         email.send()
 
 
+class ModifyTicket(BaseApiView):
+    @only_admin
+    def post(self, request):
+        ticket_id = request.data.get('ticket_id')
+        new_status = request.data.get('is_approved')
+        if ticket_id is None or new_status is None:
+            return Response('ticket_id or is_approved missed', status=status_code.HTTP_400_BAD_REQUEST)
+
+        ticket = Ticket.objects.filter(pk=ticket_id)
+        if not ticket.exists():
+            return Response('ticket with this id doesnt exist', status=status_code.HTTP_400_BAD_REQUEST)
+        t = ticket.first()
+        t.is_approved = new_status
+        t.save(update_fields=['is_approved'])
+        return Response({'id': t.pk})
+
+
 class TicketsList(BaseApiView):
     @only_admin
     def get(self, request):
-        tickets_qs = Ticket.objects.filter(is_approved=False, period__gte=datetime.datetime.now())
+        page = request.GET.get('page', 1)
+        limit = request.GET.get('limit', 150)
+
+        tickets_page = Paginator(Ticket.objects.all().order_by('id'), limit)
         tickets = [
             TicketSerializer(ticket).data
-            for ticket in tickets_qs
+            for ticket in tickets_page.page(page)
         ]
         return Response(tickets)
--- backend/core/tickets/serializers.py
+++ backend/core/tickets/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from tickets.models import Ticket
+from users.serializers import UserDetailSerializer
 
 
 class TicketSerializer(serializers.ModelSerializer):
+    user = UserDetailSerializer()
+
     class Meta:
         model = Ticket
         fields = ['id', 'resource', 'reason', 'period', 'is_approved', 'user']
