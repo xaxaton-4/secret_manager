e2e3daabf8a95b4c59308b23bedb786693ed08ef
Федоров Илья <I.Fedorov@clevermis.ru>
2025-10-04 13:05:36 +0500

    fixed init

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,36 @@
 from fastapi import FastAPI
-from .openbao_client import OpenBaoClient
+import httpx
 
 app = FastAPI()
-client = OpenBaoClient()
+OPENBAO_URL = os.getenv("OPENBAO_URL", "http://openbao:8200")
 
-@app.get("/openbao/health")
-def openbao_health():
-    return client.health()
+unseal_key = None
+
+@app.post("/init")
+async def init_vault():
+    global unseal_key
+    payload = {
+        "secret_shares": 1,
+        "secret_threshold": 1
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.post(f"{OPENBAO_URL}/v1/sys/init", json=payload)
+    data = response.json()
+    unseal_key= data["keys"][0]
+    return data
+
+@app.get("/health")
+async def check_health():
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"{OPENBAO_URL}/v1/sys/health")
+    return response.json()
+
+@app.put("/unseal")
+async def unseal_vault():
+    global unseal_key
+    if not unseal_key:
+        return {"error": "Vault is not initialized"}
+    payload = {"key": unseal_key}
+    async with httpx.AsyncClient() as client:
+        response = await client.put(f"{OPENBAO_URL}/v1/sys/unseal", json=payload)
+    return response.json()
